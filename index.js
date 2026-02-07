dfe5bb9af77a6b667aa59f8dada728b6111cf98f
HKrobinvai <hkrobin49@gmail.com>
2026-02-07 19:07:00 +0600

    add the jwt and cursor

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -9,6 +11,24 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
+
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.lfj8fm4.mongodb.net/?appName=Cluster0`;
@@ -27,19 +47,43 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db  = client.db("AshirParFoodballClub");
+    const db = client.db("AshirParFoodballClub");
 
     const playersCollection = db.collection("players");
 
 
-
-
-
-
+    // jwt route
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.JWT_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+    // logout route
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
 
     app.get("/players", async (req, res) => {
-        const players = await playersCollection.find().toArray();
-        res.send(players);
+      const players = await playersCollection.find().toArray();
+      res.send(players);
     });
 
 
@@ -53,7 +97,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-  
+
   }
 }
 run().catch(console.dir);
